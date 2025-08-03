a46076eb4a062dbb3e8cba6c072be9f4c9f22281
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-03 05:21:41 +0000

    Quando eu clico no CRM redireciona para a página inicial

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -3,10 +3,6 @@
 import React from 'react';
 import type { User } from '@/lib/types';
 import { Sidebar } from './sidebar';
-import { usePathname } from 'next/navigation';
-import CustomerChatLayout from './customer-chat-layout';
-import InternalChatLayout from './internal-chat-layout';
-import CrmLayout from '../crm/crm-layout';
 
 interface MainLayoutProps {
   user: User;
@@ -14,24 +10,6 @@ interface MainLayoutProps {
 }
 
 export function MainLayout({ user, children }: MainLayoutProps) {
-  const pathname = usePathname();
-
-  const renderContent = () => {
-    switch (pathname) {
-      case '/':
-        return <CustomerChatLayout />;
-      case '/team':
-        return <InternalChatLayout user={user} />;
-      case '/crm':
-        return <CrmLayout />;
-      // Adicione outros casos para /analytics e /settings
-      default:
-        // Por padrão, pode renderizar o children ou um layout padrão
-        return children;
-    }
-  };
-
-
   return (
     <div className="flex h-screen w-full bg-background">
       <Sidebar user={user} />
