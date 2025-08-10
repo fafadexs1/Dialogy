17298c569056b982aa81945c113b91385e679b87
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-10 05:45:29 +0000

    Error: Invalid src prop (https://mmg.whatsapp.net/o1/v/t24/f2/m234/AQPws

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'mmg.whatsapp.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
      domains: ['localhost'],
   },
