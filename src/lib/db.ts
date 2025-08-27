58a8b4580acfd75a6bf8dc340f74ae87f4613c93
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-27 01:44:11 +0000

    Use o prisma para fazer as alterações do bd dentro do supabase.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,31 +1,14 @@
+import { PrismaClient } from '@prisma/client';
 
-import { Pool } from 'pg';
-
-// Tipagem para garantir que o pool seja do tipo correto
 declare global {
   // eslint-disable-next-line no-var
-  var pool: Pool | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-let db: Pool;
+const prisma = global.prisma || new PrismaClient();
 
-// Em desenvolvimento, o "hot-reloading" pode recriar o módulo,
-// o que levaria a múltiplos pools. Usamos a variável global para prevenir isso.
-if (process.env.NODE_ENV === 'production') {
-  if (!global.pool) {
-    global.pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-    });
-  }
-  db = global.pool;
-} else {
-  // Em desenvolvimento, criamos um novo pool se ele não existir no escopo global.
-  if (!global.pool) {
-    global.pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-    });
-  }
-  db = global.pool;
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
 }
 
-export { db };
+export const db = prisma;
