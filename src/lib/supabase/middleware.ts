922e56f0cbad08c44b59564e338d436ca01f3708
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-27 03:13:28 +0000

    400 Bad Request Request Header Or Cookie Too Large nginx/1.26.3
    import

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -13,46 +13,15 @@ export async function updateSession(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value
+        getAll() {
+          return request.cookies.getAll()
         },
-        set(name: string, value: string, options: CookieOptions) {
-          // If the cookie is set, update the request cookies as well.
-          // This will make sure that the server-side pass gets the updated cookie.
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          })
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
           response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-        remove(name: string, options: CookieOptions) {
-          // If the cookie is removed, update the request cookies as well.
-          // This will make sure that the server-side pass gets the updated cookie.
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
         },
       },
     }
