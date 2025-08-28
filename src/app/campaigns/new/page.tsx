5ee847ebdf4cd0617aedfaf3b86a6a2b0cd8d39f
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-28 14:56:56 +0000

    ReferenceError: Loader2 is not defined     at NewCampaignPage (webpack-i

--- src/app/campaigns/new/page.tsx
+++ src/app/campaigns/new/page.tsx
@@ -280,7 +280,7 @@ export default function NewCampaignPage({ user }: { user: User | null }) {
   if (!user) {
     return (
         <div className="flex items-center justify-center h-full">
-            <Loader2 className="h-8 w-8 animate-spin text-primary"/>
+            <Icon.Loader2 className="h-8 w-8 animate-spin text-primary"/>
         </div>
     )
   }
