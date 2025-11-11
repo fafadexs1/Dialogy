e5d761a41454a347ba8cd6d856f515098d1b9fa4
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-11-11 04:34:06 +0000

    Quando eu clico ele vai para o /login e depois vai para / de novo, como

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,7 +18,7 @@ export default async function LoginPage({ searchParams }: LoginPageProps) {
   } = await supabase.auth.getUser();
 
   if (user) {
-    redirect('/');
+    redirect('/inbox');
   }
 
   const isRegistered = searchParams?.registered === 'true';
